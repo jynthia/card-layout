45e6111fd932183ff6037c4a7296b8debe9e5506
Cinthia Lages <cinthialgs@gmail.com>
2018-08-30 12:31:31 -0300

    Create Info.cs

---
+++ Info.cs
@@ -0,0 +1,13 @@
+namespace AdapiNotifica
+{
+    public class Info
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string FileName { get; set; }
+
+        public Info()
+        {
+        }
+    }
+}
